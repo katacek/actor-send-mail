4cf1891f7494601516fae56210c77db499f74219
JakubDrobnik <drobnik.j@gmail.com>
2017-09-04 15:06:21 +0200

    fixes, readme update

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
 # act-send-mail
 
-Apify act to send simple mail.
+Apify act to send mail.
+
+## Input
+```javascript
+{
+    data: {
+        // Email address of the recipient(s) (e.g. "Apifier <info@apifier.com>")
+        // Required
+        to: String,
+        // Email CC same format as to
+        // Required
+        cc: String
+         // Email BCC same format as to
+        bcc: String
+        // Email subject
+        // Required
+        subject: String,
+        // Text body of Email
+        // Required
+        text: String,
+    }
+}
+```
+
+## Usage
+
+### From other Apify act
+
+```javascript
+Apify.client.acts.runAct({
+    actId: 'apify/send-mail',
+    contentType: 'application/json',
+    body: JSON.stringify({
+        data: {
+            to: 'test@apifier.com',
+            subject: 'Test from act',
+            text: "Email text"
+        }
+    })
+});
+```
+
+### From Apify Crawler finish webhook
+
+For a specific crawler set the following parameters:
+
+#### Finish webhook URL (finishWebhookUrl)
+
+https://api.apifier.com/v2/acts/apify~send-mail/runs?token=APIFIER_API_TOKEN
+You can find your API token on your Apifier account page.
+
+#### Finish webhook data (finishWebhookData)
+
+```json
+{
+    "to": "test@apifier.com",
+    "subject": "Test from crawler",
+    "text": "Text"
+}
--- main.js
+++ main.js
@@ -3,37 +3,37 @@ const mailgun = require('mailgun-js');
 const request = require('request-promise');
 const typeCheck = require('type-check').typeCheck;
 const sleep = require('sleep');
+const _ = require('underscore');
 
 // Input data attributes types
 const DATA_TYPES = `{
         to: String,
         subject: String,
-        text: Maybe String,
+        text: String,
         isMock: Maybe Boolean,
+        cc: Maybe String,
+        bcc: Maybe String,
     }`;
+// Allowed mail attributes
+const MAIL_ATTRIBUTES = ['to', 'subject', 'text', 'cc', 'bcc'];
 
 Apify.main(async () => {
     // Gets input of your act
     const input = await Apify.getValue('INPUT');
     if (!input.data) {
-        console.log('Input data is missing!\nAct failed!');
-        return;
+        throw new Error('Input data is missing!');
     }
     // NOTE: Data from crawler finish webhook was JSON string
     const data = (typeof input.data === 'string') ? JSON.parse(input.data) : input.data;
     // Checks input
     if (!typeCheck(DATA_TYPES, data)) {
         console.log(`Invalid input data:\n${JSON.stringify(data)}\nData types:\n${DATA_TYPES}\nAct failed!`);
-        return;
+        throw new Error('Invalid input data');
     }
 
     // Sends mail
-    const mail = {
-        from: 'Apifier Mailer <postmaster@apify-mailer.com>',
-        to: data.to,
-        subject: data.subject,
-        text: data.text || "",
-    };
+    const mail = _.pick(data, MAIL_ATTRIBUTES);
+    mail.from = 'Apifier Mailer <postmaster@apify-mailer.com>';
 
     if (data.isMock) {
         console.log(`Mail:\n${JSON.stringify(mail)}`)
@@ -49,6 +49,4 @@ Apify.main(async () => {
     // Sleeps act for 10s
     // NOTE: We use sleep to avoid instant usage
     sleep.sleep(10);
-
-    console.log('Act finished!');
 });
