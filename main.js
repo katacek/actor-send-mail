91e714acb66002d2813f5040eba98f79219eccc0
Ondra Urban <ondrejurban@hotmail.com>
2019-03-10 16:07:46 +0100

    Fix typo

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ Apify.main(async () => {
         console.log(`Mail:\n${JSON.stringify(mail)}`)
     } else {
         const senderBody = await sender.messages().send(mail);
-        console.log(`Email with id ${senderBody.id} was send to ${mail.to}.`);
+        console.log(`Email with id ${senderBody.id} was sent to ${mail.to}.`);
     }
     // Sleeps act for 10s
     // NOTE: We use sleep to avoid instant usage
